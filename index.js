384f8a07d4d9f7cfda3572f938cf70472082a66c
Grace Durant <gdurant34@gmail.com>
2022-09-19 21:25:10 -0700

    commit tree 2dd2570c44378234ced3880d0c1495c664b1cb96

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if(block >= 42) {
+        const blocksAway = block - 42;
+        return blocksAway;
+    } else if(block < 42) {
+        const blocksAway = 42 - block;
+        return blocksAway;
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    const numberOfBlocks = distanceFromHqInBlocks(block);
+    const oneBlockInFeet = 264
+    return numberOfBlocks * oneBlockInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if(start >= destination) {
+        const oneBlockInFeet = 264;
+        return (start - destination) * oneBlockInFeet;
+    } else if(start < destination) {
+        const oneBlockInFeet = 264;
+        return (destination - start) * oneBlockInFeet;    
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const numberOfFeet = distanceTravelledInFeet(start, destination);
+    if(numberOfFeet <= 400) {
+        return 0;
+    } else if(numberOfFeet > 400 && numberOfFeet <= 2000) {
+        const billableFeet = numberOfFeet - 400;
+        return billableFeet * .02;
+    } else if(numberOfFeet > 2000 && numberOfFeet <= 2500) {
+        return 25;
+    } else if(numberOfFeet > 2500) {
+        return 'cannot travel that far';
+    }
+}
